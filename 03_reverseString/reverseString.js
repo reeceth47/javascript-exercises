d78ee3caad9b8d767cbe4e2243a7eb47aa4c01a8
reeceth47 <reeceth47@gmail.com>
2023-05-29 19:45:09 -0400

    03_reverseString complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(input) {
+    let splitString = input.split(``); 
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join(``);
+    return joinArray
+}; 
 
 // Do not edit below this line
 module.exports = reverseString;
